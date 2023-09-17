056096ecd34e34e0c56316256e8d0331d712afff
Takatoshi Matsumoto <toqoz403@gmail.com>
2023-09-17 15:00:52 +0900

    remove project specific settings

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+code --force --install-extension "ms-vsliveshare.vsliveshare"
+
 code --force --install-extension "antfu.icons-carbon"
 code --force --install-extension "antfu.theme-vitesse"
 code --force --install-extension "file-icons.file-icons"
+
+code --force --install-extension "streetsidesoftware.code-spell-checker"
+code --force --install-extension "usernamehw.errorlens"
+code --force --install-extension "github.copilot"
--- vscode/windows/install-extensions.bat
+++ vscode/windows/install-extensions.bat
@@ -1,3 +1,10 @@
+call code --force --install-extension "ms-vsliveshare.vsliveshare"
+call code --force --install-extension "ms-vscode-remote.remote-wsl"
+
 call code --force --install-extension "antfu.icons-carbon"
 call code --force --install-extension "antfu.theme-vitesse"
 call code --force --install-extension "file-icons.file-icons"
+
+call code --force --install-extension "streetsidesoftware.code-spell-checker"
+call code --force --install-extension "github.copilot"
+call code --force --install-extension "eamodio.gitlens"
