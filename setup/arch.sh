e79656a0a9ffa4568079a5a8acfbe6527e813be1
Takatoshi Matsumoto <toqoz403@gmail.com>
2023-11-23 10:26:42 +0900

    update

--- setup/arch.sh
+++ setup/arch.sh
@@ -33,25 +33,32 @@ sudo pacman -Syyuu \
   rsync \
   nodejs \
   go \
+  percona-server-clients \
+  percona-toolkit \
   aws-cli-v2
 
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
-yay -Syyuu ghq-bin aws-sam-cli-bin aws-session-manager-plugin evans-bin k6 fisher
+yay -Syyuu ghq-bin aws-sam-cli-bin aws-session-manager-plugin evans-bin k6
 
+# KidsCannon commandline-tools
 git clone https://github.com/KidsCannon/commandline-tools.git ~/.k8n/commandline-tools
 source "$HOME/.k8n/commandline-tools/init.sh"
 append-line-if-not-exists 'source "$HOME/.k8n/commandline-tools/init.sh"' ~/.bashrc
 
-# Go applications
+# go install
 go install github.com/Songmu/ghg/cmd/ghg@v0.3.0
 go install github.com/fujimura/git-gsub@v0.1.2
 
 export PATH="$HOME/go/bin:$PATH"
 append-line-if-not-exists 'PATH="$(ghg bin):$PATH"' ~/.bashrc
 append-line-if-not-exists 'PATH="$HOME/go/bin:$PATH"' ~/.bashrc
+
+# ghg get
+ghg get github.com/tkuchiki/alp@v1.0.21
+
 
 ### asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
@@ -76,7 +83,8 @@ fish -c 'fisher install rstacruz/fish-asdf'
 ## Link config
 ln -sf "$HOME/config/.gitconfig" ~/.gitconfig
 ln -sf "$HOME/config/.gitignore_global" ~/.gitignore_global
-rm -fr ~/.config/fish && ln -sf "$HOME/config/.config/fish" ~/.config/fish
+rm -fr "$HOME/.config/fish"
+ln -sf "$HOME/config/.config/fish" ~/.config/fish
 ln -sf "$HOME/config/.config/nvim" ~/.config/nvim
 ln -sf "$HOME/config/.tmux.conf" ~/.tmux.conf
 ln -sf "$HOME/config/.asdfrc" ~/.asdfrc
