6a589fe992d37f72a448fc02844969b1f688c4f2
Takatoshi Matsumoto <toqoz403@gmail.com>
2023-11-29 23:07:11 +0900

    tweaks

--- setup/arch.sh
+++ setup/arch.sh
@@ -8,57 +8,61 @@ mkdir "/tmp/$pid"
 cd "/tmp/$pid"
 
 sudo pacman -Syyuu \
+  fakeroot \
   which \
   wget \
   zip \
   unzip \
   make \
-  htop \
   whois \
+  dstat \
+  htop \
   apache \
   hey \
-  dstat \
-  git \
-  docker \
-  docker-compose \
   fish \
-  tig \
   neovim \
   tmux \
+  git \
+  tig \
+  rsync \
   fzf \
   jq \
-  mkcert \
+  docker \
+  docker-compose \
   clang \
-  ffmpeg \
-  rsync \
   nodejs \
   go \
   percona-server-clients \
   percona-toolkit \
-  aws-cli-v2
+  aws-cli-v2 \
+  mkcert \
+  ffmpeg \
+  imagemagick
 
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 
-yay -Syyuu ghq-bin aws-sam-cli-bin aws-session-manager-plugin evans-bin k6
+yay -Syyuu \
+  ghq-bin \
+  aws-sam-cli-bin \
+  aws-session-manager-plugin \
+  k6 \
+  ddosify \
+  evans-bin
 
 # KidsCannon commandline-tools
 git clone https://github.com/KidsCannon/commandline-tools.git ~/.k8n/commandline-tools
 source "$HOME/.k8n/commandline-tools/init.sh"
-append-line-if-not-exists 'source "$HOME/.k8n/commandline-tools/init.sh"' ~/.bashrc
 
 # go install
 go install github.com/Songmu/ghg/cmd/ghg@v0.3.0
 go install github.com/fujimura/git-gsub@v0.1.2
-
-export PATH="$HOME/go/bin:$PATH"
-append-line-if-not-exists 'PATH="$(ghg bin):$PATH"' ~/.bashrc
-append-line-if-not-exists 'PATH="$HOME/go/bin:$PATH"' ~/.bashrc
+PATH="$HOME/go/bin:$PATH"
+PATH="$(ghg bin):$PATH"
 
 # ghg get
 ghg get github.com/tkuchiki/alp@v1.0.21
-
 
 ### asdf
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.13.1
@@ -69,11 +73,6 @@ asdf plugin add go-sdk
 asdf plugin add flutter
 asdf plugin add erlang
 asdf plugin add elixir
-append-line-if-not-exists 'source "$HOME/.asdf/asdf.sh"' ~/.bashrc
-append-line-if-not-exists 'source "$HOME/.asdf/completions/asdf.bash"' ~/.bashrc
-
-## SDKMAN
-curl -s https://get.sdkman.io | bash
 
 ## Fisher
 fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher'
