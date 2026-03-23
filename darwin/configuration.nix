b79f1643c752454e78e376ec117f2e0df19062b1
Takatoshi Matsumoto <toqoz403@gmail.com>
2026-03-24 00:26:35 +0900

    install 1Password

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -1,4 +1,5 @@
 {
+  lib,
   ...
 }:
 {
@@ -9,13 +10,23 @@
   system.keyboard.remapCapsLockToControl = true;
   security.pam.services.sudo_local.touchIdAuth = true;
 
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "1password-cli"
+  ];
+
   # For homebrew
   system.primaryUser = "toqoz";
+
+  programs = {
+    # 1Password CLI
+    _1password.enable = true;
+  };
 
   homebrew = {
     enable = true;
 
     casks = [
+      "1password"
       "karabiner-elements"
     ];
   };
