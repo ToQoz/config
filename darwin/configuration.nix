4424d89a3581376d868c378771cc1b9044d59a0a
Takatoshi Matsumoto <toqoz403@gmail.com>
2026-03-23 22:21:20 +0900

    minimum remap + Touch ID for sudo
    - caps lock -> ctrl
    - Touch ID for sudo

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -5,4 +5,7 @@
   nixpkgs.hostPlatform = "aarch64-darwin";
   system.stateVersion = 6;
   nix.enable = false;
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToControl = true;
+  security.pam.services.sudo_local.touchIdAuth = true;
 }
