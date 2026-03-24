8f5da691014fb320a0ce6fe06b1f8dfe197260b7
Takatoshi Matsumoto <toqoz403@gmail.com>
2026-03-24 10:50:00 +0900

    nix-darwin manages home-manager

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   description = "Home Manager configuration of toqoz";
 
   inputs = {
-    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -16,24 +15,18 @@
 
   outputs =
     { nixpkgs, home-manager, nix-darwin, ... }:
-    let
-      system = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
     {
-      homeConfigurations."toqoz" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
-        modules = [ ./home.nix ];
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
-
       darwinConfigurations."remilis" = nix-darwin.lib.darwinSystem {
-        modules = [ ./darwin/configuration.nix ];
+        modules = [
+          ./darwin/configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            users.users."toqoz".home = "/Users/toqoz";
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."toqoz" = ./home-manager/home.nix;
+          }
+        ];
       };
     };
 }
