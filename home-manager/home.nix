3c276ac5df6239dca6ad283c316b4826f17fa95d
Takatoshi Matsumoto <toqoz403@gmail.com>
2026-04-08 17:27:19 +0900

    install git

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -161,10 +161,44 @@
     '';
   };
 
+  programs.git = {
+    enable = true;
 
+    settings = {
+      user = {
+        name = "Takatoshi Matsumoto";
+        email = "toqoz403@gmail.com";
+      };
+      alias = {
+        s = "!git stash list && git status -sb";
+        dw = "diff --color-words";
+        co = "checkout";
+        ci = "commit -v";
+        fi = "commit -v --fixup HEAD";
+        br = "branch";
+        wc = "whatchanged";
+        unstage = "reset HEAD --";
+        # http://qiita.com/uasi/items/f19a120e012c0c75d856
+        uncommit = "reset HEAD^";
+        recommit = "commit -c ORIG_HEAD";
+      };
 
+      core = {
+        autocrlf = "input";
+        quotepath = false;
+        precomposeunicode = true;
+        ignorecase = false;
+      };
 
+      push.default = "simple";
+      grep.lineNumber = true;
+      diff.algorithm = "histogram";
+      merge.tool = "vimdiff";
 
+      github.user = "ToQoz";
+      ghq.root = "~/src";
+    };
+  };
 
 
   programs.neovim = {
