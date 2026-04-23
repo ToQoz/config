0b49dbd7d7be6788ab564542f4682478056c9878
Takatoshi Matsumoto <toqoz403@gmail.com>
2026-04-23 10:30:56 +0900

    Remove unused .config directory

--- .config/fish/functions/select_history.fish
+++
@@ -1,9 +0,0 @@
-function select_history
-  history merge
-  history | fzf --no-sort --exact | read line
-  if test $line
-    commandline $line
-  else
-    commandline ''
-  end
-end
--- .config/fish/functions/select_repository.fish
+++
@@ -1,5 +0,0 @@
-function select_repository
-  select-ghq-list | read _selected
-  [ -n "$_selected" ]; and builtin cd "$_selected"
-  commandline -f repaint
-end
